b6b86692007181ded134a813dd4c84dd52ddbc41
lucianosants <lucianosantos.developer@gmail.com>
2023-06-16 16:52:02 -0300

    fix: update image size

--- src/components/PhotoFrame.tsx
+++ src/components/PhotoFrame.tsx
@@ -9,7 +9,7 @@ export function PhotoFrame({ source, alt }: ImageType) {
 	return (
 		<Image
 			src={source}
-			className="block w-64 bg-orange-500 h-80 rounded-xl"
+			className="block min-w-[256px] w-auto min-h-[320px] bg-orange-500 h-auto rounded-xl"
 			alt={alt}
 			height={320}
 			width={256}
